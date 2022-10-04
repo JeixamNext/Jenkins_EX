933d2a7df94b94223d470a3c6b1c7240803f9cca
jeixam <jaime.rodriguez@viewnext.com>
2022-10-04 16:04:07 +0200

    actualizacion y cambios del repo

---
+++ jenkins node.js/DLS/node-js-DSL.groovy
@@ -0,0 +1,37 @@
+job('Aplicacion Node.js DSL') {
+    description('Aplicación Node JS DSL')
+    scm {
+        git('https://github.com/JeixamNext/nodejsapp_jenkins.git', 'master') { node ->
+            node / gitConfigName('Example')
+            node / gitConfigEmail('example@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
